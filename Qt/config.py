f873041977cd636083f113fa6198736e95a61c70
Murray Stevenson <50844517+murraystevenson@users.noreply.github.com>
2026-05-23 11:27:36 -0700

    fixup! LibPNG : Update to version 1.6.58

--- Qt/config.py
+++ Qt/config.py
@@ -58,8 +58,6 @@
 			" {extraArgs}"
 			" -I {buildDir}/include -I {buildDir}/include/freetype2"
 			" -L {buildDir}/lib"
-			" --"
- 			" -DPNG_LIBRARY={buildDir}/lib/libpng{staticLibraryExtension}"
 		,
 
 		"cmake --build . --parallel {jobs} && cmake --install .",
